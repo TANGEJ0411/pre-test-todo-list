b6f8cd7471f42d8f4a023eaedaec76eaed201630
7238 唐翊傑 ejtang <11305001@pershing.com.tw>
2024-12-18 10:07:46 +0800

    fix:修正i18n使用

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import Header from "./components/Header"
 import { TodoContextProvider } from "./context/useTodoContext"
 import TodoListContent from "./views/TodoListContent"
+import { I18nextProvider } from "react-i18next"
+import i18n from "./i18n/i18n"
 
 function App() {
 
   return (
-    <TodoContextProvider>
-      <Header />
-      <TodoListContent />
-    </TodoContextProvider>
+    <I18nextProvider i18n={i18n}>
+      <TodoContextProvider>
+        <Header />
+        <TodoListContent />
+      </TodoContextProvider>
+    </I18nextProvider>
   )
 }
 
